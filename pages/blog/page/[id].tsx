7865fcbb7c527b86f77003701d684e468431f7c4
kenji <kenjix12x22@gmail.com>
2022-01-27 13:13:33 +0900

    個別のページネーション作成

---
+++ pages/blog/page/[id].tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import { Pagination } from '../../../components/Pagination';
+
+const PER_PAGE = 5; 
+
+interface Props {
+  blog: { id: number, title: string, content: string }[];
+  totalCount: number;
+}
+
+// pages/blog/[id].js
+export default function BlogPageId({ blog, totalCount }: Props) {
+  return (
+    <div>
+      <ul>
+        {blog.map(blog => (
+          <li key={blog.id}>
+            <Link href={`blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Pagination totalCount={totalCount} />
+    </div>
+  );
+}
+
+// 動的なページを作成
+export const getStaticPaths = async () => {
+  const key = {
+    headers: { 'X-MICROCMS-API-KEY': process.env.NEXT_PUBLIC_API_KEY }
+  };
+
+  const res = await fetch('https://kenji-blog.microcms.io/api/v1/blog', key)
+
+  const repos = await res.json();
+
+  const pageNumbers = [];
+
+  const range = (start:number, end:number) =>
+        [...Array(end - start + 1)].map((_, i) => start + i)
+
+  const paths = range(1, Math.ceil(repos.totalCount / PER_PAGE)).map((repo) =>  `/blog/page/${repo}`)
+
+  return { paths, fallback: false };
+};
+
+// データを取得
+export const getStaticProps = async (context: { params: { id: number }; }) => {
+  const id = context.params.id;
+
+  const key = {
+    headers: { 'X-MICROCMS-API-KEY': process.env.NEXT_PUBLIC_API_KEY }
+  };
+
+  const data = await fetch(
+    `https://kenji-blog.microcms.io/api/v1/blog?offset=${(id - 1) * 5}&limit=5`,
+    key
+  ).then(res => res.json()).catch(() => null)
+
+  return {
+    props: {
+      blog: data.contents,
+      totalCount: data.totalCount
+    }
+  };
+};
