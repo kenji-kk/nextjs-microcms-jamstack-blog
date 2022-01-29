b03fcb5c7f62c76c0acb0df74348330f712a46ba
kenji <kenjix12x22@gmail.com>
2022-01-29 21:40:52 +0900

    headerの見た目調整

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,12 +1,11 @@
 import { VFC } from 'react'
-import React from 'react';
+import Link from "next/link";
 
 export const Header:VFC = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
     <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-      <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+      <span className="font-semibold text-xl tracking-tight">KENJI-BLOG</span>
     </div>
     <div className="block lg:hidden">
       <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
@@ -15,14 +14,16 @@ export const Header:VFC = () => {
     </div>
     <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div className="text-sm lg:flex-grow">
+        <Link href='/'>
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            技術ブログ
+          </a>
+        </Link>
         <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-          Docs
-        </a>
-        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-          Examples
+          自己紹介
         </a>
         <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-          Blog
+          コンタクト
         </a>
       </div>
       <div>
