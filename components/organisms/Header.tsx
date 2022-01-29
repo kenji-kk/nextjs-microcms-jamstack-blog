4a6ad6cf2e4a5467dd4b1254525bb9582d9af7ec
kenji <kenjix12x22@gmail.com>
2022-01-29 22:17:37 +0900

    tailwind.configに@max-width定義追加、toggleボタンでヘッダー開閉機能実装

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,18 +1,23 @@
-import { VFC } from 'react'
+import { VFC, useState } from 'react'
 import Link from "next/link";
 
 export const Header:VFC = () => {
+const [isOpen, setIsOpen] = useState(false)
+const headerToggle = () => {
+  setIsOpen(!isOpen)
+}
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-purple-400 p-6">
     <div className="flex items-center flex-shrink-0 text-white mr-6">
       <span className="font-semibold text-xl tracking-tight">KENJI-BLOG</span>
     </div>
     <div className="block lg:hidden">
-      <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+              onClick={headerToggle}  >
         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
-    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto mlg:hidden">
       <div className="text-sm lg:flex-grow">
         <Link href='/'>
           <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
@@ -26,10 +31,23 @@ export const Header:VFC = () => {
           コンタクト
         </a>
       </div>
-      <div>
-        <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
-      </div>
     </div>
+    {isOpen && (<div className="w-full block flex-grow lg:hidden ">
+      <div className="text-sm ">
+        <Link href='/'>
+          <a href="#responsive-header" className="block mt-4 text-teal-200 hover:text-white mr-4">
+            技術ブログ
+          </a>
+        </Link>
+        <a href="#responsive-header" className="block mt-4 text-teal-200 hover:text-white mr-4">
+          自己紹介
+        </a>
+        <a href="#responsive-header" className="block mt-4 text-teal-200 hover:text-white">
+          コンタクト
+        </a>
+      </div>
+    </div>)}
+    
   </nav>
   
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mxl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+
+        'mlg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+
+        'mmd': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+
+        'msm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+    }},
   },
   variants: {
     extend: {},
