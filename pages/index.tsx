956c848713c5e1ae33aee47bf57afed8d3d6cd42
kenji <kenjix12x22@gmail.com>
2022-01-31 19:26:52 +0900

    スマホ用の画面の時にカラムを1列にする

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home:VFC<Props> = ({ blog, totalCount }) => {
   return (
     <Layout>
       <div className='mt-10'>
-        <ul className=' grid grid-cols-2 gap-10 w-9/12 mx-auto mb-10'>
+        <ul className=' grid lg:grid-cols-2 gap-10 w-9/12 mx-auto mb-10'>
           {blog.map(blog => (
             <Link key={blog.id} href={`/blog/${blog.id}`}>
               <a className='block'>
