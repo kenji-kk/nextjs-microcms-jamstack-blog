852f4625021c6635738872d161eae24f362312e5
kenji <kenjix12x22@gmail.com>
2022-01-31 19:15:28 +0900

    index.tsxも修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Pagination } from '../components/molecules/Pagination';
 import { Layout } from '../components/templates/Layout';
 
 type Props = {
-  blog: { id: number, title: string, content: string }[];
+  blog: { id: number, title: string, content: string, body: any }[];
   totalCount: number;
 }
 
@@ -18,16 +18,23 @@ const Home:VFC<Props> = ({ blog, totalCount }) => {
   return (
     <Layout>
       <div className='mt-10'>
-        <ul className='text-center'>
-          {blog.map((blog: Blog) => (
-            <li key={blog.id} className='mb-5'>
-              <Link href={`/blog/${blog.id}`}>
-                <a>{blog.title}</a>
-              </Link>
-            </li>
+        <ul className=' grid grid-cols-2 gap-4 w-9/12 mx-auto mb-10'>
+          {blog.map(blog => (
+            <Link key={blog.id} href={`/blog/${blog.id}`}>
+              <a className='block'>
+              <li  className='pt-5 pb-5 border-2 border-purple-400 hover:bg-teal-100'>
+                <p className='text-center pb-5 border-b border-purple-300 text-2xl'>{blog.title}</p>
+                  <div
+                    dangerouslySetInnerHTML={{
+                    __html: `${blog.body}`,}}
+                    className='mx-10 mt-5'>
+                </div>
+                </li>
+              </a>
+            </Link>
           ))}
         </ul>
-        <Pagination totalCount={totalCount} pageNum={1} />
+        <Pagination totalCount={totalCount} pageNum={1}/>
       </div>
     </Layout>
   );
